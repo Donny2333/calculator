0254bb7995360bb451ab98236ad36cf70b628e3a
Donny <1021001390@qq.com>
2017-03-26 17:50:37 +0800

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Donny on 17/3/22.
+ */
+(function () {
+    'use strict';
+
+    angular.module('calculator', [
+        'calculator.directives',
+        'calculator.services',
+        'calculator.controllers'
+    ]);
+
+}());
